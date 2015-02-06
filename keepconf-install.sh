9ae3a3c26b941c56b6be964c52f1cbeb1cf9e9e3
rfraile <rfraile@idealista.com>
2015-02-06 09:43:20 +0100

    Adding info for the removing especial files script

--- keepconf-install.sh
+++ keepconf-install.sh
@@ -17,6 +17,12 @@ if [ $? -ne 0 ]; then
 	echo "Please, install it"; exit 1
 fi
 
+file --version &> /dev/null
+if [ $? -ne 0 ]; then
+	echo "ERROR: File command not available"
+	echo "Please, install it"; exit 1
+fi
+
 pyver=`python --version 2>&1 /dev/null`
 if [ $? -ne 0 ]; then
 	echo "ERROR: Python not available"
@@ -40,13 +46,21 @@ D_BIN='/usr/bin'
 echo "Clonning repository..."
 git clone https://github.com/rfrail3/keepconf.git ${F_TMP1}
 
-echo "Creating path..."
+echo "Creating paths..."
 mkdir -p ${D_CNF}/hosts/grp
+mkdir ${D_CNF}/pre-get.d
+mkdir ${D_CNF}/post-get.d
+mkdir ${D_CNF}/pre-commit.d
+mkdir ${D_CNF}/post-commit.d
 
 echo "Copying files..."
 cp ${F_TMP1}/latest/keepconf ${D_BIN}/keepconf
 chmod 644 ${D_BIN}/keepconf
 cp ${F_TMP1}/latest/keepconf.cfg ${D_CNF}/
+cp ${F_TMP1}/latest/pre-get.d/* ${D_CNF}/pre-get.d/
+cp ${F_TMP1}/latest/post-get.d/* ${D_CNF}/post-get.d/
+cp ${F_TMP1}/latest/pre-commit.d/* ${D_CNF}/pre-commit.d/
+cp ${F_TMP1}/latest/post-commit.d/* ${D_CNF}/post-commit.d/
 cp ${F_TMP1}/latest/hosts/* ${D_CNF}/hosts/
 cp ${F_TMP1}/latest/hosts/grp/* ${D_CNF}/hosts/grp/
 
