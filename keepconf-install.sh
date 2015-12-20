d60363eeff80957ed0b568230a6bbcc9a6ca34fc
Ricardo F <rfraile@rfraile.eu>
2015-12-20 17:05:37 +0100

    Updating install script

--- keepconf-install.sh
+++ keepconf-install.sh
@@ -5,34 +5,47 @@
 # v.1.2
 #
 
+# Destination dir for configuration
+D_CNF='/etc/keepconf'
+
+# Destination dir for executable file
+D_BIN='/usr/bin'
+
+
+# Test if git is installed
 git --version &> /dev/null
 if [ $? -ne 0 ]; then
 	echo "ERROR: \"git\" command not available"
 	echo "Please, install it"; exit 1
 fi
 
+# Test if rsync is installed
 rsync --version &> /dev/null
 if [ $? -ne 0 ]; then
 	echo "ERROR: \"rsync\" command not available"
 	echo "Please, install it"; exit 1
 fi
 
+# Test if file is installed
 file /dev/null &> /dev/null  # For 01-remove-binary script
 if [ $? -ne 0 ]; then
 	echo "ERROR: \"file\" command not available"
 	echo "Please, install it"; exit 1
 fi
 
+# Test if python is installed
 pyver=`python3 --version 2>&1 /dev/null`
 if [ $? -ne 0 ]; then
 	echo "ERROR: Python not available"
 	echo "Please, install version 3"; exit 1
 else
+	# Test if version 3 of python is installed
 	pynum=`echo ${pyver} | tr -d '.''' | grep -Eo  '[0-9]*' | head -1 | cut -c 1-2`
 	if [ $pynum -lt 30 ] ; then
 		echo "ERROR: Its needed Python version 3, not ${pyver}"
 		exit 1
 	else
+                # Test if all modules needed are available
                 pymod=`python3 -c "import sys, optparse, os, glob, time, string, re, tempfile, logging, configparser, subprocess, distutils"`
                 if [ $? -ne 0 ]; then
                         echo "ERROR: Please, ensure that these Python modules are available in the local system:"
@@ -41,9 +54,8 @@ else
 	fi
 fi
 
+# Temporary dir for clone repo into it
 F_TMP1=`mktemp -d`
-D_CNF='/etc/keepconf'
-D_BIN='/usr/bin'
 
 echo "Keepconf installation script"
 echo ""
@@ -52,20 +64,21 @@ echo "Clonning repository..."
 git clone https://github.com/rfrail3/keepconf.git ${F_TMP1}
 
 echo "Creating paths..."
-mkdir -p ${D_CNF}/hosts
-mkdir -p ${D_CNF}/files
+mkdir ${D_CNF}
+mkdir ${D_CNF}/hosts
+mkdir ${D_CNF}/files
 mkdir ${D_CNF}/pre-get.d
 mkdir ${D_CNF}/post-get.d
 mkdir ${D_CNF}/pre-commit.d
 mkdir ${D_CNF}/post-commit.d
 
 echo "Copying files..."
-cp ${F_TMP1}/src/keepconf.cfg ${D_CNF}/
-cp -ar ${F_TMP1}/src/hosts/* ${D_CNF}/hosts/
-cp -ar ${F_TMP1}/src/files/* ${D_CNF}/files/
-cp ${F_TMP1}/src/post-get.d/01-remove-binary ${D_CNF}/post-get.d/
-chmod 744 ${D_CNF}/post-get.d/01-remove-binary
 cp ${F_TMP1}/src/keepconf ${D_BIN}/keepconf
+cp ${F_TMP1}/src/keepconf.cfg ${D_CNF}/
+cp -r ${F_TMP1}/src/post-get.d/* ${D_CNF}/post-get.d/
+cp -r ${F_TMP1}/src/hosts/* ${D_CNF}/hosts/
+cp -r ${F_TMP1}/src/files/* ${D_CNF}/files/
+chmod 744 ${D_CNF}/post-get.d/*
 chmod 744 ${D_BIN}/keepconf
 
 cd ${D_CNF} && ls
