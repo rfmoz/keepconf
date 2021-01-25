77e2d5d7cbaa286795bd8f07b141af02cde7c01a
Ricardo F <rfraile@rfraile.eu>
2021-01-25 20:49:55 +0100

    Refactoring output keepconf-csv2prom.sh

--- grafana-dashboard/keepconf-csv2prom.sh
+++ grafana-dashboard/keepconf-csv2prom.sh
@@ -15,12 +15,13 @@ echo keepconf_enable 1 > $dfile
 # Loop along all csv files
 for fcsv in ${dkeep}*.csv; do
 
-        # Replace underscore chars
-        fbase=`basename -s '.csv' $fcsv | tr '-' '_'`
+	# Replace underscore chars
+	fbase=$(basename -s '.csv' $fcsv | tr '-' '_')
 
-        (echo -n "keepconf_ok{file=\"$fbase\"} " && grep OK $fcsv | awk -F "\"*\"*" '{print $4}') >> $dfile
-        (echo -n "keepconf_bad{file=\"$fbase\"} " && grep BAD $fcsv | awk -F "\"*\"*" '{print $4}') >> $dfile
-        (echo -n "keepconf_fetch_time{file=\"$fbase\"} " && grep FETCH-T $fcsv | awk -F "\"*\"*" '{print $4}') >> $dfile
-        (echo -n "keepconf_commit_time{file=\"$fbase\"} " && grep COMMIT-T $fcsv | awk -F "\"*\"*" '{print $4}') >> $dfile
-        (echo -n "keepconf_total_time{file=\"$fbase\"} " && grep TOTAL-T $fcsv | awk -F "\"*\"*" '{print $4}') >> $dfile
+	{ echo -n "keepconf_ok{file=\"$fbase\"} " && grep OK $fcsv | awk -F "\"*\"*" '{print $4}'
+	  echo -n "keepconf_bad{file=\"$fbase\"} " && grep BAD $fcsv | awk -F "\"*\"*" '{print $4}'
+	  echo -n "keepconf_fetch_time{file=\"$fbase\"} " && grep FETCH-T $fcsv | awk -F "\"*\"*" '{print $4}'
+	  echo -n "keepconf_commit_time{file=\"$fbase\"} " && grep COMMIT-T $fcsv | awk -F "\"*\"*" '{print $4}'
+	  echo -n "keepconf_total_time{file=\"$fbase\"} " && grep TOTAL-T $fcsv | awk -F "\"*\"*" '{print $4}'
+	} >> $dfile
 done
